80a1d65368978fa6a1c6610b51058d4b002983ce
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-10-28 10:42:43 +0000

    add `set` method
    The method creates a new Node for the key-value pair and a hashed key for the Node' s key

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -16,6 +16,17 @@ class HashMap
     key.each_char {|char| code = (code * prime_number + char.ord) }
     code % @capacity
   end
+
+  def set(key, value)
+    node_index = hash(key)
+    raise IndexError if node_index.negative? || node_index >= @buckets.length
+    new_node = Node.new(value, key)
+  end
 end
 
 map = HashMap.new
+# initialize a set method
+# takes in a key-value pair
+# hashes the key from pair to return index for buckets list
+# if hashed key points to an empty index, put node there
+# if hashed key points to a filled index, append that node there
