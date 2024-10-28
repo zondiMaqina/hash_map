176f63a793515b4eddfd4adcbb741bfa6fe4c70f
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-10-28 10:26:00 +0000

    add hashmap class

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,6 +1,3 @@
-# initialize hash map class
-# class must keep track of: entries, buckets, collisions, capacity
-# capaciaty is the number of buckets
 class HashMap
   attr_reader :collisions, :entries
   attr_accessor :capacity, :buckets
