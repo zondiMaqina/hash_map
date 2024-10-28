0ea22282081e0f35e65c07f9b536a486a7c7be94
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-10-28 10:25:45 +0000

    add node and linked list class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,1 +1,20 @@
-puts 'hello world'
+# create linked list class
+# class must keep track of the @head
+class LinkedList
+  attr_accessor :head
+  def initialze
+    @haed = nil
+  end
+end
+
+# create node class
+# each node must keep track of its value, key, next node
+class Node
+  attr_accessor :next_node
+  attr_reader :value, :key
+  def initialize(value, key)
+    @next_node = nil
+    @value = value
+    @key = key
+  end
+end
