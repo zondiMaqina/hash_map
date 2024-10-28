6c4960f0d04451214753154569a1652bebc16615
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-10-28 10:35:03 +0000

    add hash instance method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -2,10 +2,20 @@ require_relative 'linked_list.rb'
 class HashMap
   attr_reader :collisions, :entries
   attr_accessor :capacity, :buckets
+  
   def initialize(capacity = 15)
     @capacity = capacity
     @entries = 0
     @collisions = 0
     @buckets = Array.new(@capacity)
   end
+
+  def hash(key) # produces a hash code from node's key
+    code = 0
+    prime_number = 31
+    key.each_char {|char| code = (code * prime_number + char.ord) }
+    code % @capacity
+  end
 end
+
+map = HashMap.new
