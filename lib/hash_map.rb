72ee404095042f79c867c78d394a601bcb44ac8d
zondiMaqina <zondimaqina0@gmail.com>
2024-11-01 10:43:21 +0200

    add has? method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,9 +1,11 @@
-require_relative 'linked_list.rb'
+# frozen_string_literal: true
+
+require_relative 'linked_list'
 
 class HashMap < LinkedList
   attr_reader :entries, :product
   attr_accessor :capacity, :buckets
-  
+
   def initialize(capacity = 15)
     @capacity = capacity
     @entries = 0
@@ -11,16 +13,19 @@ class HashMap < LinkedList
     @LOAD_FACTOR = 0.75
   end
 
-  def hash(key) # produces a hash code from node's key
+  # produces a hash code from node's key
+  def hash(key)
     code = 0
     prime_number = 31
-    key.each_char {|char| code = (code * prime_number + char.ord) }
+    key.each_char { |char| code = (code * prime_number + char.ord) }
     code % @capacity
   end
 
-  def set(key, value) # creates a new node and adds it to the hash map
+  # creates a new node and adds it to the hash map
+  def set(key, value)
     node_index = hash(key)
     raise IndexError if node_index.negative? || node_index >= @buckets.length
+
     @entries += 1
     new_node = Node.new(value, key)
     add_node(node_index, new_node, key) # adds node to hash map
@@ -32,7 +37,7 @@ class HashMap < LinkedList
     if bucket.nil?
       @buckets[node_index] = new_node
       LinkedList.new.head = new_node # makes first node the head of linked list
-    elsif bucket != nil && bucket.key != key
+    elsif !bucket.nil? && bucket.key != key
       current_node = bucket
       append(new_node, current_node)
     elsif bucket.key == key
@@ -40,10 +45,11 @@ class HashMap < LinkedList
     end
   end
 
-  def get(key) # returns the value of the node with the given key
+  # returns the value of the node with the given key
+  def get(key)
     node_index = hash(key)
     bucket = @buckets[node_index]
-    if bucket == nil
+    if bucket.nil?
       nil
     elsif bucket.key == key
       bucket.next_node = nil
@@ -53,13 +59,27 @@ class HashMap < LinkedList
     end
   end
 
+  def has?(key)
+    index = hash(key)
+    bucket = @buckets[index]
+    if bucket == nil
+      false
+    else
+      until bucket.next_node == nil || bucket.key == key
+        bucket = bucket.next_node
+      end
+      false if bucket.key != key
+      true if bucket.key == key
+    end
+  end
+
   def resize
     buckets_copy = []
-    @buckets.each {|bucket| buckets_copy << bucket if bucket != nil}
+    @buckets.each { |bucket| buckets_copy << bucket unless bucket.nil? }
     @buckets.clear
     @capacity *= 2
     @buckets = Array.new(@capacity)
-    buckets_copy.each {|bucket| @buckets[hash(bucket.key)] = bucket}
+    buckets_copy.each { |bucket| @buckets[hash(bucket.key)] = bucket }
   end
 
   def product
@@ -69,18 +89,18 @@ end
 
 test = HashMap.new
 test.set('apple', 'red')
- test.set('banana', 'yellow')
- test.set('carrot', 'orange')
- test.set('dog', 'brown')
- test.set('elephant', 'gray')
- test.set('frog', 'green')
- test.set('grape', 'purple')
- test.set('hat', 'black')
- test.set('ice cream', 'white')
- test.set('jacket', 'blue')
- test.set('kite', 'pink')
- test.set('lion', 'golden')
-p test.buckets.size
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+p test.has?('elephant')
 # add the resize method
 # method should spread the nodes to their appropiate indexes
 
